ba4a44d6259dc62c899914139fad4ca41bc13b44
OhTalDoDuzz <edufecae@gmail.com>
2025-10-17 00:35:42 -0300

    Subida backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,19 +4,15 @@ import fichaRoutes from './API/ficha_api.js';
 
 const app = express();
 
-// Habilita o uso de JSON no corpo das requisições
 app.use(express.json());
 
-// Rotas principais
 app.use('/users', userRoutes);
 app.use('/fichas', fichaRoutes);
 
-// Rota padrão de teste
 app.get('/', (req, res) => {
   res.send('Servidor rodando normalmente 🚀');
 });
 
-// Inicializa o servidor
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
